3387ae44411802d4c1723aaf9311266b19fa3338
Yusadolat <Yusadolat@gmail.com>
2018-06-03 16:12:08 +0100

    First Commit

---
+++ www/js/jplay.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	var stream = {
+		title: " Dr Abdul Raqqqaz Alaro",
+		mp3: "http://88.202.230.180:8000/;"
+	},
+	ready = false;
+
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function (event) {
+			ready = true;
+			$(this).jPlayer("setMedia", stream);
+		},
+		pause: function() {
+			$(this).jPlayer("clearMedia");
+		},
+		error: function(event) {
+			if(ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+				// Setup the media stream again and play it.
+				$(this).jPlayer("setMedia", stream).jPlayer("play");
+			}
+		},
+		swfPath: "dist/jplayer",
+		supplied: "mp3",
+		preload: "none",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		keyEnabled: true
+	});
+
+});
